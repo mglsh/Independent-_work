ff2d91a94b5453fe36942d4e79eec0354c8e9669
Misha <mglsh@ukr.net>
2021-04-07 22:13:41 +0300

    Start

--- main_samost1.py
+++ main_samost1.py
@@ -5,7 +5,7 @@ print('start')
 # Красиві графіки
 plt.style.use('ggplot')
 # Розмір зображень
-plt.rcParams['figure.figsize'] = (15, 5)
+plt.rcParams['figure.figsize'] = (15, 10)
 
 fixed_df = pd.read_csv('la-crimes.csv', sep=',', encoding='latin1',
                        parse_dates=['Date Reported'], dayfirst=True,
@@ -26,7 +26,18 @@ print('Завдання 3')  # Завдання №3
 t3 = fixed_df['Victim Descent'].value_counts()
 print(t3[:1])
 
+print('Завдання 4')  # Завдання №4
+t4 = fixed_df[['Area Name', 'Reporting District']][:10].copy()
+print(t4)
+t41=t4.sort_values(by="Reporting District", ascending=False)
+print(t41)
 
+t4.plot('Area Name','Reporting District', kind='bar')
+plt.show()
+print('Завдання 5')  # Завдання №5
+t5 = fixed_df[['Area Name', 'Victim Sex']][:10].copy()
+
+print(t5.groupby('Area Name').sum())
 
 #fixed_df[['Crime Code Description', 'Victim Sex']].plot(kind='bar')
 #plt.show()
